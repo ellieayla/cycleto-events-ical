dcfbc389a1c0f6e97320bf7809f09323ed6b9848
ellieayla <1447600+me@users.noreply.github.com>
2025-02-21 16:39:08 -0500

    Set user agent more

--- cycleto/settings.py
+++ cycleto/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "cycleto.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "cycleto (+https://github.com/ellieayla/cycleto-events-ical)"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15 ICalExport (+https://github.com/ellieayla/cycleto-events-ical)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
